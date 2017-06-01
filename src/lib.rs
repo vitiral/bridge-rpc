7c07fe11788fbc92ae5cad27594f469eddf4e409
Garrett Berg <vitiral@gmail.com>
2017-06-01 15:17:06 -0600

    design arch

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+struct Context {
+    x: u16,
+}
+
+struct Bar {
+    cx: Context,
+}
+
+struct Baz {
+    cx: Context,
+}
+
+
+enum State {
+    Bar(Bar),
+    Baz(Baz),
+}
+
+
+impl Baz {
+    fn process_stuff(self) -> State {
+        // do some stuff, note that the state
+        // could be anything here
+        State::Bar(Bar{ cx: self.cx })
     }
+}
+
+
+#[test]
+fn it_works() {
 }
